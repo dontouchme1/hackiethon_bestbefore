105b3b1b4381022110dfb45413f1c2f80af80ca5
dontouchme1 <tanya91329@gmail.com>
2025-03-28 23:03:00 +1100

    change the design of app.jsx

--- showcase-site/src/App.jsx
+++ showcase-site/src/App.jsx
@@ -1,52 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Navbar from './navbar'
-import Dropdown from './Dropdown'
-import DropZone from './Dropzone'
+import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ReceiptPage from "./pages/ReceiptPage";
 import CapturePage from "./pages/CapturePage";
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-  let MyUrl = "";
-  if (MyUrl){
-    return (
-      <div className="App">
-        <Navbar />
-        <DropZone url={MyUrl}/>
-      </div>
-    )
-  }
-
   return (
-    <>
-    <div className="App" style={{ backgroundImage: 'url(/stars.png)', height: '100vh' }}>
-        <Navbar />
-        <div className="relative text-center mt-10">
-          <h1 className="outline-text">
-            SHOWCASE
-          </h1>
-          <h2 className="main-title">
-            Hackiethon Widget Showcase
-          </h2>
-        </div>
-
-        <Routes>
+    <div className="widget-wrapper">
+      <div className="app-card">
+        <div className="page-content">
+          <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/receipts" element={<ReceiptPage />} />
             <Route path="/capture" element={<CapturePage />} />
           </Routes>
-        <DropZone/>
+        </div>
+
+        <nav className="bottom-nav">
+          <Link to="/receipts">📋</Link>
+          <Link to="/">🏠</Link>
+          <Link to="/capture">📷</Link>
+        </nav>
+      </div>
     </div>
-    <nav className="bottom-nav">
-        <Link to="/receipts">📋</Link>
-        <Link to="/">🏠</Link>
-        <Link to="/capture">📷</Link>
-    </nav>
-    
-  </>
-  );      
+  );
 }
+
 export default App;
--- showcase-site/src/pages/HomePage.jsx
+++ showcase-site/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import FoodListSwiper from "./FoodListSwiper";
+import "./HomePage.css"; // 確保有掛 CSS
 
 export default function HomePage() {
-    return (
-      <div>
-        <h1>🍱 到期食物清單</h1>
-        <FoodListSwiper />
-      </div>
-    );
-  }
+  return (
+    <div className="homepage-container">
+      <h1 className="homepage-title">🍱 到期食物清單</h1>
+      <FoodListSwiper />
+    </div>
+  );
+}
